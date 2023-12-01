8a09dbac26d8d91331a783535b5d564a5807b1e0
jiesir <jiesir16@gmail.com>
2023-12-01 09:58:36 +0800

    UAD:导包方式修改

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use leetcode::solutions::{*};
+use leetcode::solutions;
 
 fn main() {
     let mut vect = vec![0, 1, 0, 3, 12];
-    lc_283_move_zeros::move_zeroes(&mut vect);
+    solutions::lc_283_move_zeros::move_zeroes(&mut vect);
     println!("{:?}", vect);
 
     let args = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
-    let max_area = lc_11_container_with_most_water::max_area(args);
+    let max_area = solutions::lc_11_container_with_most_water::max_area(args);
     println!("---------max_area:{max_area}");
 
     let args = vec![-1, 0, 1, 2, -1, -4];
-    let result = lc_15_3sum::three_sum(args);
+    let result = solutions::lc_15_3sum::three_sum(args);
     println!("---result:{:?}", result)
 }
 
