8fa894d4935c3863c3499269a39ba0dc5958a47c
jiesir <jiesir16@gmail.com>
2023-12-22 19:12:17 +0800

    ADD:leetcode#560#和为k的子数组,解法优化

--- src/solutions/lc_560_subarray_sum_equals_k.rs
+++ src/solutions/lc_560_subarray_sum_equals_k.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+#[deprecated]
 pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
     let mut l_idx = 0;
     let mut result = 0;
@@ -9,5 +12,26 @@ pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
         }
         l_idx += 1;
     }
+    result
+}
+
+pub fn subarray_sum2(nums: Vec<i32>, k: i32) -> i32 {
+    let mut pre_sum_map = HashMap::new();
+    // 给个初始值
+    let mut pre_sum = 0;
+    pre_sum_map.insert(pre_sum,1);
+    let mut result = 0;
+
+    let mut i = 0;
+    while i < nums.len() {
+        pre_sum += nums[i];
+        let pre_key = pre_sum - k;
+        if pre_sum_map.contains_key(&pre_key) {
+            result += pre_sum_map.get(&pre_key).unwrap();
+        }
+        *pre_sum_map.entry(pre_sum).or_insert(0) += 1;
+        i += 1;
+    }
+
     result
 }
