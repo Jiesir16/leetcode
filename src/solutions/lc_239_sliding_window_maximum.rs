e893f5db763535eb77f3ef1217c0ddb240ef56d1
jiesir <jiesir16@gmail.com>
2024-01-05 18:01:27 +0800

    UPDATE:leetcode#239#滑动窗口最大值,优化

--- src/solutions/lc_239_sliding_window_maximum.rs
+++ src/solutions/lc_239_sliding_window_maximum.rs
@@ -8,24 +8,29 @@ pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
 
     while r_idx < nums.len() {
         let r_num = nums[r_idx];
-        // 队尾入
-        while !dequeue.is_empty() && (dequeue.back().unwrap() - r_num < 0) {
-            dequeue.pop_back();
-        }
-        dequeue.push_back(r_num);
-
+        push_queue_last(&mut dequeue, r_num);
 
         if r_idx + 1 >= k as usize {
             res.push(*dequeue.front().unwrap());
-            // 队头出
-            if dequeue.front().unwrap() == &nums[l_idx] {
-                dequeue.pop_front();
-            }
+            pop_queue_front(&mut dequeue, nums[l_idx]);
             l_idx += 1;
         }
         r_idx += 1;
     }
-
-
     res
+}
+
+/// 队尾入
+fn push_queue_last(dequeue: &mut VecDeque<i32>, r_num: i32) {
+    while !dequeue.is_empty() && (dequeue.back().unwrap() - r_num < 0) {
+        dequeue.pop_back();
+    }
+    dequeue.push_back(r_num);
+}
+
+/// 队头出
+fn pop_queue_front(dequeue: &mut VecDeque<i32>, l_num: i32) {
+    if dequeue.front().unwrap() == &l_num {
+        dequeue.pop_front();
+    }
 }
